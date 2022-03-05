f13f0265a9de8d386374e12de95b5b6601db5963
TRudenko22 <timothy.rudenkom@gmail.com>
2022-03-05 06:01:33 -0500

    Cleaned everything up

--- main.py
+++ main.py
@@ -2,10 +2,15 @@ import yaml
 import datetime
 from datetime import date
 
+
+sDate = lambda index: datetime.datetime\
+        .strptime(index, '%m/%d/%Y').date()
+
+
 # Sets todays date.
-dt = datetime.datetime
 today = datetime.datetime.today()
 today_delta = date(year=today.year, month=today.month, day=today.day)
+
 
 # Pulls in the YAML file.
 with open("assign.yml", "r") as file:
@@ -16,29 +21,28 @@ cs_date = due_dates['cSharp']
 net_date = due_dates['Net1']
 win_date = due_dates['Windows']
 
-# assigns the value from the 'due' key and parses it as a date 
-cDue = dt.strptime(cs_date[0]['due'], '%m/%d/%Y').date()
-nDue = dt.strptime(net_date[0]['due'], '%m/%d/%Y').date()
-wDue = dt.strptime(win_date[0]['due'], '%m/%d/%Y').date()
 
-print(f"You have some C# assignments due in {str(cDue - today_delta).split(', ')[0]}\n")
+# assigns the value from the 'due' key and parses it as a date 
+cDue = sDate(cs_date[0]['due'])
+nDue = sDate(net_date[0]['due'])
+wDue = sDate(win_date[0]['due'])
+
+CS_DueDate = str(cDue - today_delta).split(', ')[0]
+NW_DueDate = str(nDue - today_delta).split(', ')[0]
+W_DueDate = str(wDue - today_delta).split(', ')[0]
+
+print(f"You have some C# assignments due in {CS_DueDate}\n")
+
 [print('  -  ', x) for x in cs_date[0]['task'].split(' ')]
 print('\n')
 
-print(f"You have some Network assignments due in {str(nDue - today_delta).split(', ')[0]}\n")
+print(f"You have some Network assignments due in {NW_DueDate}\n")
+
 [print('  -  ', x) for x in net_date[0]['task'].split(' ')]
 print('\n')
 
-print(f"You have some Windows assignments due in {str(wDue - today_delta).split(', ')[0]}\n")
+print(f"You have some Windows assignments due in {W_DueDate }\n")
+
 [print('  -  ', x) for x in win_date[0]['task'].split(' ')]
 print('\n')
-#
-#cSharp = []
-#for item in due_dates["cSharp"]:
-#    date = datetime.datetime.strptime(item['due'], '%m/%d/%Y').date()
-#    print(f"""
-#    Due : {str(date - today_delta).split(', ')[0]}
-#    Date : {date.strftime('%m/%d/%Y')}
-#    Task : {item['task']}
-#          
-#    """)
+
